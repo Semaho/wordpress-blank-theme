1f1040417f1444feeaf428c987c2f60dc1dde6de
Sébastien Vignol <sebastien@vignol.be>
2020-10-12 22:20:02 +0200

    New Gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,114 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    postcss      = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssnano      = require('gulp-cssnano'),
-    uglify       = require('gulp-uglify'),
-    concat       = require('gulp-concat'),
-    sourcemaps   = require('gulp-sourcemaps');
-    //flatten      = require('gulp-flatten');
+const gulp         = require('gulp'),
+      babel        = require('gulp-babel'),
+      sass         = require('gulp-sass'),
+      postcss      = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      cssnano      = require('gulp-cssnano'),
+      uglify       = require('gulp-uglify'),
+      concat       = require('gulp-concat'),
+      sourcemaps   = require('gulp-sourcemaps'),
+      browserSync  = require('browser-sync').create();
+
+const localURL     = 'https://dev.yournewsite.com';
+    
+/**
+ * Sources & destinations.
+ */
+
+const paths = {
+    scss: {
+        src: './assets/sass/**/*.scss', // Will be used by `src()` and `watch()`.
+        dest: './assets/dist/css/'      // Will be used by `dest()`.
+    },
+    js: {
+        src: 'assets/js/**/*.js',  // Will be used by `src()` and `watch()`.
+        dest: 'assets/dist/js/'    // Will be used by `dest()`.
+    },
+    php: {
+        src: '**/*.php'
+    }
+};
+
+
+/**
+ * Init browser-sync.
+ */
+
+function serve(done)
+{
+    browserSync.init({
+        proxy: localURL,
+        port: 8080
+    });
+    done();
+}
+
+
+/**
+ * Reload browser-sync.
+ */
+
+function reload(done) {
+    browserSync.reload();
+    done();
+}
 
 
 /*
  * Processing & compiling styles.
  */
 
-gulp.task('styles', function() {
-    return gulp.src('./assets/sass/*')
+function styles()
+{
+    return gulp.src(paths.scss.src)
         .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
-            .pipe(postcss([ autoprefixer() ]))
-            .pipe(concat('style.min.css'))
-            .pipe(cssnano())
-            //.pipe(flatten()) // Ignore directory hierarchy.
-        .pipe(sourcemaps.mapSources(function(sourcePath, file) {
-            return '../../sass/' + sourcePath;
-        }))
-        .pipe(sourcemaps.write('../../sass/maps'))
-        .pipe(gulp.dest('./assets/dist/css'));
-});
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(concat('style.min.css'))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('./maps')) 
+        .pipe(gulp.dest(paths.scss.dest))
+        .pipe(browserSync.stream())
+    ;
+}
 
 
 /*
  * Processing JS.
  */
 
-gulp.task('scripts', function() {
+function scripts()
+{
     return gulp.src([
             //'./node_modules/slick-carousel/slick/slick.js', // Uncomment import @ bottom of style.scss
-            //'./assets/js/burger.js',
-            './assets/js/seb.js'
+            './node_modules/jquery/dist/jquery.slim.min.js',
+            paths.js.src
         ])
         .pipe(sourcemaps.init())
+        .pipe(babel({
+			presets: ['@babel/preset-env']
+		}))
         .pipe(concat('scripts.min.js'))
         .pipe(uglify())
-        .pipe(sourcemaps.mapSources(function(sourcePath, file) {
-            return '../../js/' + sourcePath;
-        }))
-        .pipe(sourcemaps.write('../../js/maps'))
-        .pipe(gulp.dest('./assets/dist/js'));
-});
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(paths.js.dest))
+        .pipe(browserSync.stream())
+    ;
+}
 
 
 /*
  * Watch tasks.
  */
 
-gulp.task('default', function()
+function watchAll()
 {
-    gulp.watch("assets/sass/*", gulp.series('styles'));
-    gulp.watch("assets/js/*", gulp.series('scripts'));
-});
+    gulp.watch(paths.scss.src, gulp.series(styles));
+    gulp.watch(paths.js.src, gulp.series(scripts));
+    gulp.watch(paths.php.src, gulp.series(reload));
+}
+
+exports.styles  = styles;
+exports.scripts = scripts;
+exports.build   = gulp.parallel(styles, scripts);
+exports.default = gulp.series(serve, watchAll);
